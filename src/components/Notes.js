d21cae9aedc55f6aec6e5a0f0deff6bc584b2dd2
Prasath Govindaraju <govindarajuprasath@gmail.com>
2025-10-01 18:19:59 -0300

    Simple Task Manager App : Enhanced the Notes Component

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -9,10 +9,45 @@ export const Notes = () => {
                 <div className="header">
                     <h1>Notes</h1>
                 </div>
-                <li>React Concepts convered : Properties Drilling, Multiple Components
-                    with parent-child relationship, useState Hook for state management,
-                    onClick Event Handling, Conditional Rendering, List Rendering, Component
-                    Reusability, Basic CSS for styling.
+                <li>React Concepts convered : 
+                    1. React useState Hook
+                    2. React Properties and Properties Drilling
+                    3. Building React Component Tree
+                    4. React Conditional Rendering via Boolean useState
+                    5. React List Rendering 
+                    6. React Component Reusability and Component Modularization
+                    7. Basic CSS for styling
+                    8. React useRef Hook
+                    9. React Fragments
+                    10. React Forms and Controlled Components
+                    11. React Component Composition
+                    12. React Keys in Lists
+                    13. React Lifting State Up
+                    14. React Strict Mode
+                    15. React Component Communication
+                </li>
+                <li>
+                   Javascript Concepts covered in this App :
+                    1. Javascript ES6 Arrow Functions
+                    2. Javascript ES6 let and const keywords
+                    3. Javascript ES6 Template Literals
+                    4. Javascript ES6 Object Destructuring
+                    5. Javascript ES6 Array Spread Operator
+                    6. Javascript ES6 Array Filter and Map functions
+                    7. Javascript ES6 Modules - import and export
+                    8. Javascript Math.random() function
+                    9. Javascript Event Handling
+                    10. Javascript Forms and Form Validations
+                    11. Javascript DOM Manipulations
+                    12. Javascript Array and Object Manipulations
+                    13. Javascript Console API
+                    14. Javascript Functions and Function Parameters
+                    15. Javascript Data Types
+                    16. Javascript JSON
+                    17. Javascript Callbacks
+                    18. Javascript Promises
+                    19. Javascript Async and Await
+                    20. Javascript Local Storage
                 </li>
           </ul>
         </section>
